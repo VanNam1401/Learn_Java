ae070ae1e00f86dc78157f148e63865c1c60f348
Your Name <you@example.com>
2022-11-02 21:09:57 +0700

    update

---
+++ Java_39b/src/main.java
@@ -0,0 +1,16 @@
+
+public class main {
+public static void main(String[] args) {
+	ConNguoi cNguoi = new ConNguoi("TITV", 2021);
+//	cNguoi.an();
+//	cNguoi.ngu();
+//	cNguoi.uong();
+	
+	HocSinh hSinh = new HocSinh("TITV", 2021, "Lớp 1", "Trường 1");
+	hSinh.an();
+	hSinh.uong();
+	hSinh.ngu();
+	hSinh.lamBaiTap();
+	System.out.println(hSinh.getTenTruong());
+}
+}
